7359a6c0f296ce26d0bea09ebae47ffda474955c
ch4n3-yoon <ch4n3.yoon@gmail.com>
2020-02-20 21:26:59 +0900

    Add anit bot-detecting function to getContentInfo.py

--- getContentInfo.py
+++ getContentInfo.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import random
+import string
 from bs4 import BeautifulSoup
 
 
@@ -30,9 +32,16 @@ class Content:
         self.get_title()
 
     def get_raw_content(self):
+        user_agents = [
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41',
+        ]
+
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+            'User-Agent': user_agents[random.randint(0, len(user_agents) - 1)],
+            'Referer': 'http://web.humoruniv.com/board/humor/list.html?table=com',
         }
         r = requests.get(self.content_url, headers=headers)
         r.encoding = 'euc-kr'
